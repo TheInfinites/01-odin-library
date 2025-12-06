29b06428bca2831933472ac8eaaf4dc853d4babe
TheInfinites <90694663+TheInfinites@users.noreply.github.com>
2025-12-06 10:47:53 +0530

    basic logic and structure

---
+++ script.js
@@ -0,0 +1,185 @@
+const myLibrary = [];
+
+
+
+// the book contructors 
+function Book (name, author, pages, readStatus) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    // this.readStatus = readStatus;
+    this.id = crypto.randomUUID()
+
+    if (readStatus.toLowerCase() === "yes") {
+        this.readStatus = "read";
+    } else {
+        this.readStatus = "yet to read";
+    };
+
+}
+
+// adding books to the lib
+function addBookToLibary(name, author, pages, readStatus) {
+    const book = new Book(name, author, pages, readStatus);
+    myLibrary.push(book); 
+}
+
+addBookToLibary("Percy Jackson", "Rick", 234, "Nope" );
+addBookToLibary("meow", "meo", 234, "yes" );
+addBookToLibary("Jack", "Jill", 234, "yes" );
+
+
+const bla = document.querySelector(".hero");
+
+function creatingDivs(title, author, pages, idd, read) {
+    const heroCard = document.createElement("div")
+    heroCard.className = `heroCard`;
+    heroCard.id = `h${idd}`;
+    bla.appendChild(heroCard);
+    const getHeroCard = document.querySelector(`#h${idd}`);
+
+    // the title
+    const title_container = document.createElement("div");
+    title_container.classList = `title_container`;
+    title_container.id = `t${idd}`;
+    getHeroCard.appendChild(title_container);
+    const getTitleContainer = document.querySelector(`#t${idd}`)
+
+
+    const title_pre = document.createElement("div");
+    title_pre.classList = "book_pre";
+    title_pre.textContent = "Title";
+    getTitleContainer.appendChild(title_pre);
+
+    // title main
+    const titleInfo = document.createElement("div");
+    titleInfo.className = "titleInfo";
+    titleInfo.textContent = title;
+    getTitleContainer.appendChild(titleInfo);
+
+    //////////////
+
+    // author
+    const author_container = document.createElement("div");
+    author_container.classList = `author_container`;
+    author_container.id = `a${idd}`;
+    getHeroCard.appendChild(author_container);
+    const getAuthorContainer = document.querySelector(`#a${idd}`)
+
+
+    const author_pre = document.createElement("div");
+    author_pre.classList = "author_pre";
+    author_pre.textContent = "Author";
+    getAuthorContainer.appendChild(author_pre);
+
+
+    // author main
+    const authorInfo = document.createElement("div");
+    authorInfo.className = "authorInfo";
+    authorInfo.textContent = author;
+    getAuthorContainer.appendChild(authorInfo);
+
+    ///////////////
+
+    // pages
+    const pages_container = document.createElement("div");
+    pages_container.classList = `pages_container`;
+    pages_container.id = `p${idd}`;
+    getHeroCard.appendChild(pages_container);
+    const getPagesContainer = document.querySelector(`#p${idd}`)
+
+
+    const pages_pre = document.createElement("div");
+    pages_pre.classList = "pages_pre";
+    pages_pre.textContent = "Pages";
+    getPagesContainer.appendChild(pages_pre);
+
+
+    // pages main
+    const pagesInfo = document.createElement("div");
+    pagesInfo.className = "pagesInfo";
+    pagesInfo.textContent = pages;
+    getPagesContainer.appendChild(pagesInfo);
+
+    /////////////////////////////
+
+    // read status
+    const readStatus_container = document.createElement("div");
+    readStatus_container.classList = `readStatus_container`;
+    readStatus_container.id = `r${idd}`;
+    getHeroCard.appendChild(readStatus_container);
+    const getReadStatusContainer = document.querySelector(`#r${idd}`)
+    getReadStatusContainer.dataset.id = `${idd}`;
+
+
+    const read_pre = document.createElement("div");
+    read_pre.classList = "read_pre";
+    read_pre.textContent = "Read Status";
+    getReadStatusContainer.appendChild(read_pre);
+
+
+    // read main
+    const readInfo = document.createElement("div");
+    readInfo.className = "readInfo";
+    readInfo.textContent = read;
+    getReadStatusContainer.appendChild(readInfo);
+    
+}
+
+function loopinThrough () {
+    let i = 1;
+    const something = myLibrary.forEach(book => {
+  
+        console.log(i)
+        creatingDivs(book.name, book.author, book.pages, book.id, book.readStatus);
+        i++;
+    })
+
+    // for (let i = 0; i < myLibrary.length; i++){
+    //     creatingDivs(myLibrary[i].name,myLibrary[i].author, myLibrary[i].id )
+    // }
+}
+
+// console.log(loopinThrough());
+
+const form = document.querySelector(".form-book");
+
+
+// getting and setting data from the form 
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    
+    const book_name = document.querySelector("#book_name").value;
+    const author_name = document.querySelector("#author_name").value;
+    const pages = document.querySelector("#pages").value;
+    const readStatus = document.querySelector("#status").value;
+
+    // const getHeroCard = document.querySelector(".heroCard");
+    // getHeroCard.remove();
+    myLibrary.length = 0;
+    addBookToLibary(book_name, author_name, pages, readStatus);
+    loopinThrough();
+
+});
+
+
+loopinThrough();
+
+
+myLibrary.forEach(stuff => {
+    const getRead = document.querySelectorAll(".readStatus_container");
+
+    getRead.forEach(read => {
+        read.addEventListener("mouseup", (e) => {
+            console.log("yes!")
+            // console.log(stuff.id);
+
+
+            if(stuff.id == read.dataset.id){
+                console.log("yay!");
+            }
+
+        })
+    })
+
+})
